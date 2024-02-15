e7984d0262e22876e20866647a3a416ad22ae5a1
Anderson Duboc <andersonduboc@gmail.com>
2024-02-15 11:31:54 -0300

    new script params

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PROJECT_ID="your-project-id"
+PROJECT_ID=$(gcloud config list --format='value(core.project)')                                                                                                      py cli-code-api 11:26:18
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
 
 gcloud config set project ${PROJECT_ID}
 
@@ -10,4 +11,13 @@ gcloud services enable \
     cloudfunctions.googleapis.com \
     run.googleapis.com \
     cloudbuild.googleapis.com \
-    artifactregistry.googleapis.com
+    artifactregistry.googleapis.com \
+    secretmanager.googleapis.com
+
+gcloud secrets create pr-secret-id \
+    --replication-policy="automatic"
+
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com \
+    --role=roles/secretmanager.secretAccessor
